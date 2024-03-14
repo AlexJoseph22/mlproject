ca03e82754c9294d0edf0c91a7f561523c22733d
Alex Joseph <alex.joseph.2207@gmail.com>
2024-03-14 14:11:50 +0530

    Model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -31,7 +31,7 @@ class DataTransformation:
 
             categorical_features = [
                 "gender",
-                "ethnicity",
+                "race_ethnicity",
                 "parental_level_of_education",
                 "lunch",
                 "test_preparation_course"
@@ -78,28 +78,23 @@ class DataTransformation:
 
             target_column_name = "math_score"
 
-            numerical_features = ["writing_score", "reading_score"]
-
-            categorical_features = [
-                "gender",
-                "ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course"
-            ]
-
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
 
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
             target_feature_test_df = test_df[target_column_name]
+            
+            logging.info("Applying preprocessing object on training and testing dataframes")
+
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
-                input_feature_train_df, np.array(target_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_df)
             ]
 
             test_arr = np.c_[
-                input_feature_test_df, np.array(target_feature_test_df)
+                input_feature_test_arr, np.array(target_feature_test_df)
             ]
             
             logging.info(f"Saved Preprocessing Object")
---
+++ src/components/model_trainer.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from dataclasses import dataclass
+from src.utils import evaluate_models
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    RandomForestRegressor,
+    GradientBoostingRegressor
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                "random_forest": RandomForestRegressor(random_state=42),
+                "decision_tree": DecisionTreeRegressor(random_state=42),
+                "gradient_boosting": GradientBoostingRegressor(random_state=42),
+                "linear_regression": LinearRegression(),
+                "k_nearest_neighbors": KNeighborsRegressor(),
+                "xgb": XGBRegressor(),
+                "catboost": CatBoostRegressor(random_state=42, verbose=0),
+                "adaboost": AdaBoostRegressor(random_state=42)
+            }
+
+            model_report = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, 
+                                           y_test=y_test, models=models)
+            
+            best_model_score = max(sorted(list(model_report.values())))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("The best performing model has a score less than 0.6")
+            logging.info("Best model selected")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            return best_model_score
+        except Exception as e:
+            raise CustomException(e, sys)
